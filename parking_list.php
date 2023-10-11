676f3efc81cbe9d4a1ba2d7cf7615fee9025ab81
Gints Kristaps Perkons <gintskristaps13@gmail.com>
2023-10-11 20:12:15 +0300

    added filter

--- parking_list.php
+++ parking_list.php
@@ -1,5 +1,6 @@
 <?php include("src/php/db.php") ?>
 <?php include("src/php/statements.php") ?>
+<?php include("src/php/filter.php") ?>
 <?php include("src/php/sorters.php") ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,19 +55,21 @@
         <div class="filters-container">
             <div class="filter">
                 <?php
-                if (isset($_GET["sortBy"])) {
+                if (isset($_GET["filter"])) {
                     ?>
-                    <select name="sort-by" id="sort-by" class="dropdown" onchange="doReload({'sortBy':this.value});">
+                    <select name="sort-by" id="sort-by" class="dropdown" onchange="doReload({'filter':this.value});">
                         <option style="display:none;" selected>Sort By</option>
-                        <option value="all">All spots</option>
-                        <option value="distance">Zero-Cost spots</option>
-                        <option value="price" <?php echo ($_GET["sortBy"] == "priceAsc" || $_GET["sortBy"] == "priceDesc") ? "selected" : "" ?>>Pay-Per-Hour</option>
-                        <option value="disabled">Disabled spots</option>
+                        <option value="all" <?php echo $_GET["filter"] == "all" ? "selected" : "" ?>>All spots</option>
+                        <option value="distance" <?php echo $_GET["filter"] == "distance" ? "selected" : "" ?>>Zero-Cost spots
+                        </option>
+                        <option value="price" <?php echo $_GET["filter"] == "price" ? "selected" : "" ?>>Pay-Per-Hour</option>
+                        <option value="disabled" <?php echo $_GET["filter"] == "disabled" ? "selected" : "" ?>>Disabled spots
+                        </option>
                     </select>
                     <?php
                 } else {
                     ?>
-                    <select name="sort-by" id="sort-by" class="dropdown" onchange="doReload({'sortBy':this.value});">
+                    <select name="sort-by" id="sort-by" class="dropdown" onchange="doReload({'filter':this.value});">
                         <option style="display:none;" selected>Sort By</option>
                         <option value="all">All spots</option>
                         <option value="distance">Zero-Cost spots</option>
@@ -78,7 +81,8 @@
                 ?>
             </div>
             <div class="filter">
-                <input type="text" name="search" id="search" placeholder="Search for address" class="input">
+                <input type="text" name="search" id="search" placeholder="Search for address" class="input"
+                    value='<?php echo isset($_GET["q"]) ? $_GET["q"] : "" ?>'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="search-icon">
                     <path fill="rgb(var(--text-color))"
                         d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.612 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3l-1.4 1.4ZM9.5 14q1.875 0 3.188-1.313T14 9.5q0-1.875-1.313-3.188T9.5 5Q7.625 5 6.312 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14Z" />
@@ -90,10 +94,10 @@
             $getParkingList->execute();
             $getParkingList->setFetchMode(PDO::FETCH_ASSOC);
             $result = $getParkingList->fetchAll();
-            if (isset($_GET["sortBy"])) {
+            if (isset($_GET["filter"])) {
                 try {
-                    if ($_GET["sortBy"]) {
-                        usort($result, "sortBy" . ucfirst($_GET["sortBy"]));
+                    if ($_GET["filter"] == "price") {
+                        usort($result, "sortByPriceAsc");
                     }
                 } catch (\Throwable $th) {
                     //throw $th;
@@ -102,7 +106,14 @@
             }
             foreach ($result as $row) {
                 if (isset($_GET["filter"])) {
-                    if (!str_contains(strtolower($row["address"]), strtolower($_GET["filter"]))) {
+                    echo filterIsValid($row,$_GET["filter"]);
+                    if (!filterIsValid($row,$_GET["filter"])) {
+                        continue;
+                    }
+                }
+                
+                if (isset($_GET["q"])) {
+                    if (!str_contains(strtolower($row["address"]), strtolower($_GET["q"]))) {
                         continue;
                     }
                 }
@@ -155,34 +166,23 @@
 </body>
 
 <script>
-    document.getElementsByClassName("search-icon")[0].addEventListener("click", (e) => { doReload({ "filter": e.view.document.getElementById("search").value }) })
+    document.getElementsByClassName("search-icon")[0].addEventListener("click", (e) => { doReload({ "q": e.view.document.getElementById("search").value }) })
     document.getElementById("search").addEventListener("keyup", (e) => {
         if (e.code == "Enter") {
-            doReload({ "filter": e.target.value })
+            doReload({ "q": e.target.value })
         }
     })
 
     function doReload(reloadData) {
         const urlParams = new URLSearchParams(window.location.search);
         console.log(reloadData)
-        const sortByURL = urlParams.get('sortBy');
-        const filterURL = urlParams.get('filter');
-        if (reloadData["sortBy"]) {
-            if (reloadData["sortBy"] == "price") {
-                urlParams.set('sortBy', "priceAsc");
-            }
-            if (reloadData["sortBy"] == "distance") {
-                urlParams.set('sortBy', "distance");
-            }
-            if (reloadData["sortBy"] == "all") {
-                urlParams.set('sortBy', "all");
-            }
-            if (reloadData["sortBy"] == "disabled") {
-                urlParams.set('sortBy', "disabled");
-            }
-        }
-        if (reloadData["filter"] != undefined) {
+        const sortByURL = urlParams.get('filter');
+        const filterURL = urlParams.get('q');
+        if (reloadData["filter"]) {
             urlParams.set('filter', reloadData["filter"]);
+        }
+        if (reloadData["q"] != undefined) {
+            urlParams.set('q', reloadData["q"]);
         }
         window.location.search = urlParams.toString();
     }
